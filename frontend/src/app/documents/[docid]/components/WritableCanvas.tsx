5167040c93612113f62bbebfa12d0abbe33ec1b3
Pearse Hutson <pearse.2008@gmail.com>
2024-04-29 16:23:49 +0000

    Bold and Code

--- frontend/src/app/documents/[docid]/components/WritableCanvas.tsx
+++ frontend/src/app/documents/[docid]/components/WritableCanvas.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
-import { Descendant, createEditor } from "slate";
+import { useCallback, useState } from "react";
+import { Descendant, Editor, Transforms, createEditor, Element } from "slate";
 import { Editable, Slate, withReact } from "slate-react";
 // This example is for an Editor with `ReactEditor` and `HistoryEditor`
 import { BaseEditor } from "slate";
 import { ReactEditor } from "slate-react";
 import { HistoryEditor } from "slate-history";
 
-type CustomElement = { type: "paragraph"; children: CustomText[] };
+type ParagraphElement = { type: "paragraph"; children: CustomText[] };
+type CodeElement = { type: "code"; children: CustomText[] };
+
+type CustomElement = ParagraphElement | CodeElement;
 type CustomText = { text: string; bold?: true };
 
 declare module "slate" {
@@ -17,22 +20,133 @@ declare module "slate" {
   }
 }
 
+// Define a React component renderer for our code blocks.
+const CodeElement = (props) => {
+  return (
+    <pre {...props.attributes}>
+      <code>Code: {props.children}</code>
+    </pre>
+  );
+};
+
+const DefaultElement = (props) => {
+  return <p {...props.attributes}>Default: {props.children}</p>;
+};
+
+const Leaf = (props) => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? "bold" : "normal" }}
+    >
+      {props.children}
+    </span>
+  );
+};
+
+const CustomEditor = {
+  isBoldMarkActive(editor) {
+    const marks = Editor.marks(editor);
+    return marks ? marks.bold === true : false;
+  },
+
+  isCodeBlockActive(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: (n) => n.type === "code",
+    });
+
+    return !!match;
+  },
+
+  toggleBoldMark(editor) {
+    const isActive = CustomEditor.isBoldMarkActive(editor);
+    if (isActive) {
+      Editor.removeMark(editor, "bold");
+    } else {
+      Editor.addMark(editor, "bold", true);
+    }
+  },
+
+  toggleCodeBlock(editor) {
+    debugger;
+    const isActive = CustomEditor.isCodeBlockActive(editor);
+    Transforms.setNodes(
+      editor,
+      { type: isActive ? null : "code" },
+      { match: (n) => Element.isElement(n) && Editor.isBlock(editor, n) }
+    );
+  },
+};
+
+const initialValue: Descendant[] = [
+  {
+    type: "paragraph",
+    children: [
+      {
+        text: "A line of text in a paragraph.",
+      },
+    ],
+  },
+];
 export default function WritableCanvas() {
   const [editor] = useState(() => withReact(createEditor()));
 
-  const initialValue: Descendant[] = [
-    {
-      type: "paragraph",
-      children: [
-        {
-          text: "A line of text in a paragraph.",
-        },
-      ],
-    },
-  ];
+  const renderElement = useCallback((props: { element: { type: any } }) => {
+    switch (props.element.type) {
+      case "code":
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
+  const renderLeaf = useCallback((props) => {
+    return <Leaf {...props} />;
+  }, []);
+
   return (
     <Slate editor={editor} initialValue={initialValue}>
-      <Editable />
+      <div>
+        <button
+          className="rounded-full bg-lime-300"
+          onMouseDown={(event) => {
+            event.preventDefault();
+            CustomEditor.toggleBoldMark(editor);
+          }}
+        >
+          Bold
+        </button>
+        <button
+          className="rounded-full bg-lime-300"
+          onMouseDown={(event) => {
+            event.preventDefault();
+            CustomEditor.toggleCodeBlock(editor);
+          }}
+        >
+          Code Block
+        </button>
+      </div>
+      <Editable
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        onKeyDown={(event) => {
+          if (!event.ctrlKey) {
+            return;
+          }
+          switch (event.key) {
+            case "`": {
+              event.preventDefault();
+              CustomEditor.toggleCodeBlock(editor);
+              break;
+            }
+            case "b": {
+              event.preventDefault();
+              CustomEditor.toggleBoldMark(editor);
+              break;
+            }
+          }
+        }}
+      />
     </Slate>
   );
 }
